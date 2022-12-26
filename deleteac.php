2b500072889535d8ad31fdb17e82dbe71dbe8adc
MohdNawaz <99327926+Mohammedgithubb@users.noreply.github.com>
2022-12-26 11:52:11 +0530

    Update deleteac.php

--- deleteac.php
+++ deleteac.php
@@ -1,4 +1,5 @@
-<?php
+delete.php(create new file to delete)
+
 include('session.php');
 ?>
 <!DOCTYPE html>
@@ -19,9 +20,13 @@ setInterval(function(){ countdown(); },1000);
 </script>
 <?php
 $id=$loggedin_id;
+$temp = $id;
 $sql="DELETE FROM member WHERE mem_id='$id'";
 $result=mysqli_query($con,$sql);
 if($result){
+    $sql = "INSERT INTO deleted_member(mem_id) values ('$temp')";
+    $delete = mysqli_query($con,$sql);
+
  echo " <div align='center'>";
  echo "Account Deleted Sucessfully.";
  echo " <a href='index.php' >Click here</a> to go back. ";
